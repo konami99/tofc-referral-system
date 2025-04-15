ca4d0ce1a4b331d998c14eec86fafa0d8dfe06cc
Richard Chou <konami99@hotmail.com>
2025-04-16 00:05:12 +1000

    updated users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,18 +7,40 @@ RSpec.describe UsersController, type: :controller do
     end
 
     describe 'referral code exists' do
+      let(:action) do
+        post :create, params: { user: { name: 'test', email: 'test@g.com', referral_code: u2.referral_code } }
+      end
+
       it 'should create a new user' do
         expect {
-          post :create, params: { user: { name: 'test', email: 'test@g.com', referral_code: u2.referral_code } }
+          action
         }.to change { User.count }.by(1)
+      end
+
+      it 'should create correct referral' do
+        action
+        user = User.find_by(email: 'test@g.com')
+
+        expect(user.referrer).to eq u2
       end
     end
 
     describe 'referral code not provided' do
+      let(:action) do
+        post :create, params: { user: { name: 'test', email: 'test@g.com' } }
+      end
+
       it 'should create a new user' do
         expect {
-          post :create, params: { user: { name: 'test', email: 'test@g.com' } }
+          action
         }.to change { User.count }.by(1)
+      end
+
+      it 'should not create' do
+        action
+        user = User.find_by(email: 'test@g.com')
+
+        expect(user.referrer).to be_nil
       end
     end
   end
