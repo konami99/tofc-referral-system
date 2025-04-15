1f5aa2722f7109e1e0a2cf83f45442d9deb2e879
Richard Chou <konami99@hotmail.com>
2025-04-15 20:36:36 +1000

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  belongs_to :referrer, class_name: 'User',
+  belongs_to :referrer, class_name: "User",
                             foreign_key: :referred_by_id,
                             optional: true,
                             inverse_of: :referrals
 
-  has_many :referrals, class_name: 'User', dependent: :destroy, inverse_of: :referrer                      
+  has_many :referrals, class_name: "User", dependent: :destroy, inverse_of: :referrer
 
-  
+  validates_uniqueness_of :referral_code
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,22 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:u1) do
+    User.build(
+      name: 'richard',
+      email: 'h@gmcil.com'
+    )
+  end
+
+  let(:u2) do
+    User.build(
+      name: 'bill',
+      email: '2@gmail.com'
+    )
+  end
+
+  let(:u3) do
+    User.build(
+      name: 'tom',
+      email: 't@gmail.com'
+    )
+  end
+
+  before do
+    u1.referrer = u3
+    u2.referrer = u3
+  end
+
   describe 'associations' do
-    it 'should link to referrer' do
-      u1 = User.build(
-        name: 'richard',
-        email: 'h@gmcil.com'
-      )
-      u2 = User.build(
-        name: 'bill',
-        email: '2@gmail.com'
-      )
+    it 'should link to correct referrer' do
+      expect(u1.referrer).to be u3
+    end
 
-      u1.referrer = u2
-
-      expect(u2.referrals).to include u1
+    it 'should link to correct referrals' do
+      expect(u3.referrals).to contain_exactly u1, u2
     end
   end
 
+  describe 'validations' do
+    describe 'referral_codes are unique' do
+      before do
+        u1.referral_code = 'abc'
+        u2.referral_code = '456'
+      end
 
+      it 'should not throw error if referral_code are different' do
+        expect(u1.save).to be true
+        expect(u2.save).to be true
+      end
+    end
+
+    describe 'referral_codes are not unique' do
+      before do
+        u1.referral_code = 'abc'
+        u2.referral_code = 'abc'
+      end
+
+      it 'should not throw error if referral_code are different' do
+        expect(u1.save).to be true
+        expect(u2.save).to be false
+      end
+    end
+  end
 end
